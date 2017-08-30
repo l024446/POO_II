663afd0e00422e1302aa5a7796fa7e805a9081f2
l024446 <30847574+l024446@users.noreply.github.com>
2017-08-30 17:05:32 -0300

    Add files via upload

---
+++ Projeto Flor do Norte/src/projeto/flor/pkgdo/norte/Projeto Flor do Norte/src/projeto/flor/pkgdo/norte/Cliente.java
@@ -0,0 +1,28 @@
+package projeto.flor.pkgdo.norte;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Esta é a superclasse Cliente, ela posteriormente será usada para construir
+ * as subclasses ClienteFisica e ClienteJuridica se referindo à clientes que
+ * sejam pessoa física ou pessoa jurídica respectivamente.
+ * @author l024446
+ */
+public class Cliente {
+
+    private int id; 
+    private String nome;
+    private String endereço;
+    private String telefone;
+    
+    public Cliente (String nome, String endereço, String telefone){
+        this.nome = nome;
+        this.endereço = endereço;
+        this.telefone = telefone;
+        }
+}
+
