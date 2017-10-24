56980ed3786d284987f15d6a7e78149338a81cd4
Unknown <l024446@g.unicamp.br>
2017-10-24 19:25:24 -0200

    a atualização das classes

--- Projeto Flor do Norte/src/projeto/flor/pkgdo/norte/Funcionario.java
+++ Projeto Flor do Norte/src/projeto/flor/pkgdo/norte/Funcionario.java
@@ -9,7 +9,7 @@ package projeto.flor.pkgdo.norte;
 /**
  * A classe funcionaário será utilizada para gerenciar as informações dos
  * funcionários da empresa. Tanto as informações de contato quanto as qualifica-
- * ções e informações necessárias para pagamento
+ * ções e informações necessárias para pagamento.
  * @author l024446
  */
 public class Funcionario {
@@ -17,11 +17,60 @@ public class Funcionario {
     private String nome;
     private String endereço;
     private final String cpf;
+    private String dadosbancarios;
+
+    public String getDadosbancarios() {
+        return dadosbancarios;
+    }
+
+    public void setDadosbancarios(String dadosbancarios) {
+        this.dadosbancarios = dadosbancarios;
+    }
+    private int idgerente;
+
+    public int getIdgerente() {
+        return idgerente;
+    }
+
+    public void setIdgerente(int idgerente) {
+        this.idgerente = idgerente;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+    
      
-    public Funcionario(String nome, String endereço, String cpf){
+    public Funcionario(String nome, String endereço, String cpf, int idgerente, String dadosbancarios){
         this.nome = nome;
         this.endereço = endereço;
         this.cpf = cpf;
+        this.idgerente = idgerente;
+        this.dadosbancarios = dadosbancarios;
     }
-    
+       
 }
